bbb97518d25b2ec1fb1a8f6123ca060ea736dad8
JISOO LEE <67191178+bazzang@users.noreply.github.com>
2024-05-20 23:26:31 +0900

    modified:   package-lock.json 	modified:   package.json 	modified:   src/container/header.js 	modified:   src/css/menu.css

--- src/container/header.js
+++ src/container/header.js
@@ -7,10 +7,15 @@ export const Header = () => {
     React.useEffect(() =>{
 
     })
+
+    const onClickLogin = () => {
+        console.log('onClickLogin(header)');
+    }
+
     return (
         <div className='header-area'>
             <div style={{width:"500px"}}>로그인, 회원가입 / 마이페이지, 로그아웃 들어갈 자리 </div>
-            
+            <div className='btn' onClick={onClickLogin}>login</div>
         </div>
     )
 }
