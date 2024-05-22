a5d9d9584fcfc5d77572f97b2e38f74312e69b79
JISOO LEE <67191178+bazzang@users.noreply.github.com>
2024-05-22 23:50:40 +0900

    modified:   package-lock.json 	modified:   package.json 	modified:   src/container/main/login.js

--- src/container/main/login.js
+++ src/container/main/login.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import '../css/header.css';
+// import '../css/header.css';
+import axios from 'axios';
+import swiper from 'swiper';
 
 export const Login = () => {
     const [form, setForm] = React.useState({
@@ -7,6 +9,8 @@ export const Login = () => {
         pw : "" 
     });
     
+    const [userId, setUserId] = React.useState("");
+    const [userPw, setUserPw] = React.useState("");
     
     React.useEffect(() =>{
         
@@ -14,7 +18,24 @@ export const Login = () => {
 
     const onClickLogin = () => {
         console.log('onClickLogin(Login)');
+        // try {
+        // // POST 요청은 body에 실어 보냄
+        //     axios.post('/user', {
+        //         id: userId,
+        //         pw: userPw
+        //     }).then(response => console.log('response', response));
+        // } catch (e) {
+        //     console.error(e);
+        // }
         
+    }
+
+    const onChangeId = (e) => {
+        setUserId(e.target.value);
+    }
+
+    const onChangePw = (e) => {
+        setUserPw(e.target.value);
     }
 
     return (
@@ -22,9 +43,11 @@ export const Login = () => {
             <div style={{width:"500px"}}>로그인, 회원가입 / 마이페이지, 로그아웃 버튼 들어갈 자리 </div>
             <div className='btn' onClick={onClickLogin}>login</div>
             <label>아이디</label>
-            <input type='text'></input>
+            <input type='text' onChange={onChangeId} />
+            
             <label>비밀번호</label>
-            <input type='password'></input>
+            <input type='password' onChange={onChangePw} />
+            
         </div>
     )
 }
